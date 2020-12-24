cac1c1db4cadb5f0a9241f40ef26b5edf1f14a46
Mark Bucciarelli <mkbucc@gmail.com>
2020-12-24 06:56:14 -0500

    spring initializer output

---
+++ src/main/java/com/markbucciarelli/hotwiredemochat/HotwireDemoChatApplication.java
@@ -0,0 +1,13 @@
+package com.markbucciarelli.hotwiredemochat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotwireDemoChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotwireDemoChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/markbucciarelli/hotwiredemochat/HotwireDemoChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.markbucciarelli.hotwiredemochat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotwireDemoChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
