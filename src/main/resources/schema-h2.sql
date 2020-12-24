8531ec32c3383420c4f7b2cc1f3ef2c20ac81147
Mark Bucciarelli <mkbucc@gmail.com>
2020-12-24 08:07:15 -0500

    Add database schema

---
+++ src/main/resources/schema-h2.sql
@@ -0,0 +1,19 @@
+-- You chat about a particular topic in a room.
+CREATE TABLE rooms (
+  id         IDENTITY      NOT NULL PRIMARY KEY,
+  name       VARCHAR(100)  NOT NULL,
+  created_at DATE          NOT NULL,
+  updated_at DATE          NOT NULL
+);
+
+-- A room has many messages.
+CREATE TABLE messages (
+  id         IDENTITY      NOT NULL PRIMARY KEY,
+  room_id    INT           NOT NULL,
+  content    VARCHAR(500)  NOT NULL,
+  created_at DATE          NOT NULL,
+  updated_at DATE          NOT NULL,
+
+  FOREIGN KEY (room_id) REFERENCES rooms(id)
+)
+;
--- src/test/java/com/markbucciarelli/hotwiredemochat/HotwireDemoChatApplicationTests.java
+++ src/test/java/com/markbucciarelli/hotwiredemochat/HotwireDemoChatApplicationTests.java
@@ -1,13 +1,26 @@
 package com.markbucciarelli.hotwiredemochat;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 
 @SpringBootTest
 class HotwireDemoChatApplicationTests {
 
 	@Test
+	// No need for an assert---if the context doesn't load this test fails.
+	@SuppressWarnings("java:S2699")
 	void contextLoads() {
+	}
+
+
+	@Test
+	@Sql({"/schema-h2.sql"})
+	// No need for an assert---if the SQL schema is invalid, this test fails.
+	@SuppressWarnings("java:S2699")
+	void testH2Schema() {
 	}
 
 }
