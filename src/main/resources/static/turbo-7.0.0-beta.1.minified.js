c6c726eba66ee7bb254fee954eca0444714c9012
Mark Bucciarelli <mkbucc@gmail.com>
2020-12-24 07:19:44 -0500

    Add turbo.js from https://cdn.skypack.dev/@hotwired/turbo
    See https://turbo.hotwire.dev/handbook/installing.

---
+++ src/main/resources/static/turbo-7.0.0-beta.1.minified.js
@@ -0,0 +1,7 @@
+/*
+ * Skypack CDN - @hotwired/turbo@7.0.0-beta.1
+ * Remove "min" from url for additional help & instructions.
+ */
+
+export * from '/-/@hotwired/turbo@v7.0.0-beta.1-tPCW1AHTtVGOe5r89LWv/dist=es2020,min/@hotwired/turbo.js';
+export {default} from '/-/@hotwired/turbo@v7.0.0-beta.1-tPCW1AHTtVGOe5r89LWv/dist=es2020,min/@hotwired/turbo.js';
