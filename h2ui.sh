c46992d9248104ac650b35d5e2d7e2b6425f4f31
Mark Bucciarelli <mkbucc@gmail.com>
2020-12-26 18:09:35 -0500

    Import CSS and JS on every page
    Also,
      * Define spring.datasource.platform so schema-h2.sql is actually run.
      * Modify create table so it only runs if table does not exist.  Now,
      jar will create H2 database tables if it doesn't exist and will start
      without error if the DB tables do exist.
      * Add h2ui.sh shell script to spin up an H2 UI.  It assumes
      version 1.4.200 exists in the local Maven repository.

---
+++ h2ui.sh
@@ -0,0 +1,3 @@
+#! /bin/sh -
+
+java -jar ~/.m2/repository/com/h2database/h2/1.4.200/h2-1.4.200.jar
--- src/main/resources/schema-h2.sql
+++ src/main/resources/schema-h2.sql
@@ -1,5 +1,5 @@
 -- You chat about a particular topic in a room.
-CREATE TABLE rooms (
+CREATE TABLE IF NOT EXISTS rooms (
   id         IDENTITY      NOT NULL PRIMARY KEY,
   name       VARCHAR(100)  NOT NULL,
   created_at BIGINT        NOT NULL,
@@ -7,7 +7,7 @@ CREATE TABLE rooms (
 );
 
 -- A room has many messages.
-CREATE TABLE messages (
+CREATE TABLE IF NOT EXISTS messages (
   id         IDENTITY      NOT NULL PRIMARY KEY,
   room_id    INT           NOT NULL,
   content    VARCHAR(500)  NOT NULL,
